0d412c568cb059384da4e82155a52279344b7d89
Folfy <folfy@users.noreply.github.com>
2019-03-11 20:18:09 +0100

    updated dotfiles; bugfixed tmate

--- tmate.sh
+++ tmate.sh
@@ -7,16 +7,17 @@ sock="/tmp/tmate_$CON.sock"
 # Provide the shown ID as parameter to fetch the full url and connect
 
 server() {
-	tmate -S $SOCK new-session -d
-	tmate -S $SOCK wait tmate-ready
-	t_ssh="$(tmate -S $SOCK display -p '#{tmate_ssh}')"
-	t_web="$(tmate -S $SOCK display -p '#{tmate_web}')"
+	tmate -S $sock new-session -d
+	tmate -S $sock wait tmate-ready
+	t_ssh="$(tmate -S $sock display -p '#{tmate_ssh}')"
+	t_web="$(tmate -S $sock display -p '#{tmate_web}')"
 	res="$(curl -H "Max-Days: 1" --upload-file <(echo -e "$t_ssh\n$t_web") "https://upfile.sh/$CON")"
 	id="$(echo "$res" | sed 's/.*dow":"\([^"]*\)".*/\1/' | cut -d/ -f4)"
 	echo "$res"
 	echo "t_ssh: $t_ssh"
 	echo "t_web: $t_web"
 	echo "ID: $id"
+	read -p "Press any key to exit..."
 }
 
 client() { 
@@ -25,6 +26,7 @@ client() {
 	t_ssh="$(echo "$res" | grep "^ssh")"
 	echo "$res"
 	echo "t_ssh: $t_ssh"
+	read -p "Press any key to connect now..."
 	eval "$t_ssh"
 }
 
